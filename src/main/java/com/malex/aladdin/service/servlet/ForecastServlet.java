300203b38d5ff7c89948332fbc506b6e2e0c0b34
Alex Manusovich <alex.manusovich@mozido.com>
2015-02-12 20:27:25 -0600

    web

--- src/main/java/com/malex/aladdin/service/servlet/ForecastServlet.java
+++ src/main/java/com/malex/aladdin/service/servlet/ForecastServlet.java
@@ -13,7 +13,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.ByteBuffer;
 
@@ -21,6 +20,10 @@ import java.nio.ByteBuffer;
  * Created by Alex Manusovich on 1/21/15.
  */
 public class ForecastServlet extends HttpServlet {
+
+    public static final int START_HOUR = 1;
+    public static final int END_HOUR = 13;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
         URL url;
@@ -28,46 +31,57 @@ public class ForecastServlet extends HttpServlet {
         BufferedReader br;
 
         response.setHeader("Content-Type", "application/octet-stream");
-        response.setHeader("Content-Length", "" + 4 * 12);
+        response.setHeader("Content-Length", "" + 3 * 4 * 12);
 
         try {
             url = new URL("https://api.forecast.io/forecast/a1b3feeab00e064da02d075a4d29e82f/30.267153,-97.743061");
-            is = url.openStream();  // throws an IOException
+
+            is = url.openStream();
             br = new BufferedReader(new InputStreamReader(is));
 
             JSONParser jsonParser = new JSONParser();
             try {
                 JSONObject jsonObject = (JSONObject) jsonParser.parse(br);
                 JSONArray hourly = (JSONArray) ((JSONObject) jsonObject.get("hourly")).get("data");
-                for (int i = 1; i < 13; i++) {
+
+                for (int i = START_HOUR; i < END_HOUR; i++) {
                     JSONObject hour = (JSONObject) hourly.get(i);
-                    Object temperature = hour.get("temperature");
-                    int temp = 0;
-                    if (temperature instanceof Long) {
-                        temp = (int) ((Long) temperature * 100);
-                    } else {
-                        temp = (int) ((Double) temperature * 100);
-                    }
-                    byte[] bytes = ByteBuffer.allocate(4).putInt(temp).array();
-                    response.getOutputStream().write(bytes);
-                    System.out.println(temp);
+                    writeParameter(response, hour, "apparentTemperature", 100);
+                    writeParameter(response, hour, "windSpeed", 100);
+                    writeParameter(response, hour, "precipIntensity", 1000);
                 }
+
                 response.getOutputStream().flush();
                 response.getOutputStream().close();
             } catch (ParseException e) {
                 e.printStackTrace();
             }
 
-        } catch (MalformedURLException mue) {
-            mue.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             try {
                 if (is != null) is.close();
             } catch (IOException ioe) {
-                // nothing to see here
+                ioe.printStackTrace();
             }
         }
+    }
+
+    private void writeParameter(final HttpServletResponse response,
+                                final JSONObject hour,
+                                final String dataKey,
+                                final int q) throws IOException {
+        int temp;
+
+        Object temperature = hour.get(dataKey);
+        if (temperature instanceof Long) {
+            temp = (int) ((Long) temperature * q);
+        } else {
+            temp = (int) ((Double) temperature * q);
+        }
+
+        byte[] bytes = ByteBuffer.allocate(4).putInt(temp).array();
+        response.getOutputStream().write(bytes);
     }
 }
